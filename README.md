d0aead5d346afe3f6af021810a32330c9c477f52
liyi <Rong Li>
2018-05-20 16:41:20 +0800

    init version

---
+++ README.md
@@ -0,0 +1,11 @@
+# ProductCompare
+WatchGuard product compare
+
+# Module Design
+0. Configuration // read config from with T/M series names
+1. SelectOptions //get compare link SelectOption , DoSelect()
+2. ParsePage //parse the compare page to get data
+3. CsvWriter // persistance of csv format
+4. Sort
+
+
---
+++ SelectTest.py
@@ -0,0 +1,41 @@
+# -*- coding: UTF-8 -*-
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+import json
+
+products = []
+
+# read config
+with open("series.json") as json_file:
+    json_data = json.load(json_file)
+    print(json_data)
+
+# get product list
+driver = webdriver.Chrome()
+driver.get('https://www.watchguard.com/wgrd-products/appliances-compare')
+
+for s in json_data[u'series']:
+    newOptions = driver.find_elements_by_css_selector('#p1 optgroup[label="' + s + '"] option')
+    for option in newOptions:
+        products.append(option.text)
+
+
+print products, type(products[0])
+
+# iterate over all product and get compare url
+for x in range(0,len(products),3):
+    driver.get('https://www.watchguard.com/wgrd-products/appliances-compare')
+
+    print type(products[x])
+    print "xpath is %s" % '//select[@id="p1"]/*/option[text()="' + products[x].decode("utf-8") + '"]'
+    driver.find_element_by_xpath('//select[@id="p1"]/*/option[text()="' + products[x].decode("utf-8") + '"]').click()
+    driver.find_element_by_xpath('//select[@id="p2"]/*/option[text()="' + products[x+1].decode("utf-8") + '"]').click()
+    driver.find_element_by_xpath('//select[@id="p3"]/*/option[text()="' + products[x+2].decode("utf-8") + '"]').click()
+
+    # submit our compare request
+    driver.find_element_by_xpath('//form[@id="products_select_form"]/input').click()
+    print "url after we submit =  %s" % driver.current_url
+
+driver.quit()
+
+
